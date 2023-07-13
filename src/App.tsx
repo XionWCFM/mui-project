04dae9020455a3632cab714bd4e138bb3b6c532b
xion <calf-love@naver.com>
2023-07-14 04:19:19 +0900

    :sparkles: Init
    init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import PopoverExample from './components/ui-components/PopoverExample';
+
+const App = () => {
+  return (
+    <div>
+      <PopoverExample />
+    </div>
+  );
+};
+
+export default App;
